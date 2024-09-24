c8249f8aa6242bf474ddc62d85388f9bbc713da2
jikai-senpai <thejasondutys@hotmail.com>
2024-09-24 13:07:23 -0400

    Diseno del sistema

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 require_once 'src/Includes/Functions.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Acción: Matar</title>
+</head>
+<body>
+    <h1>Contratar sicario</h1>
+    <label for="id">Nombre del sicario:</label>
+    <input>
+    <br>
+    <h1>Seleccionar objetivo</h1>
+    <label for="id">Nombre del objetivo:</label>
+    <select>
+
+    </select>
+    <br>
+    <br>
+    <br>
+    <button>Matar</button>
+
+</body>
+</html>
